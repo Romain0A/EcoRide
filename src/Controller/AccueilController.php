c3b2324138813545d0eff53f3cd372dfcb9c094e
Romain Auburtin <romain.auburtin@gmail.com>
2026-02-19 20:16:19 +0100

    ajout des mention légale ainsi que de contact

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -13,4 +13,16 @@ final class AccueilController extends AbstractController
     {
         return $this->render('accueil.html.twig');
     }
+
+    #[Route('/mention', name: 'app_mentionLegale')]
+    public function mentionLegale(): Response
+    {
+        return $this->render('mentionLegale.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
 }
